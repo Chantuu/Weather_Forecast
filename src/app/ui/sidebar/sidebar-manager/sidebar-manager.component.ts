703e7ac2d9fec9444d8b88901969bacc903ad5a0
g.tchanturia <47988187+Chantuu@users.noreply.github.com>
2025-06-14 23:28:52 +0400

    Create SidebarManager Component

---
+++ src/app/ui/sidebar/sidebar-manager/sidebar-manager.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-manager',
+  imports: [],
+  templateUrl: './sidebar-manager.component.html',
+  styleUrl: './sidebar-manager.component.css'
+})
+export class SidebarManagerComponent {
+
+}
