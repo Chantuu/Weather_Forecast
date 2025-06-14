a4cecef4abbfcc41984add86a5d7fd2151898faa
g.tchanturia <47988187+Chantuu@users.noreply.github.com>
2025-06-14 23:32:43 +0400

    Create SidebarButtonManager Component

---
+++ src/app/ui/sidebar/sidebar-button-manager/sidebar-button-manager.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-button-manager',
+  imports: [],
+  templateUrl: './sidebar-button-manager.component.html',
+  styleUrl: './sidebar-button-manager.component.css'
+})
+export class SidebarButtonManagerComponent {
+
+}
